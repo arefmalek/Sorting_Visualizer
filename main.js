eef53b6c846577ff80b062612d0309367dc200d2
arefmalek <arefmalek02@gmail.com>
2021-10-12 00:12:59 -0400

    just to sync up

--- main.js
+++ main.js
@@ -12,12 +12,13 @@ const CANVAS_WIDTH = canvas.width;
 const CANVAS_HEIGHT= canvas.height;
 
 var ctx = canvas.getContext("2d");
-ctx.fillStyle="#FF0000";
+ctx.fillStyle="red";
 
 resetArray();
 var box_width = Math.floor(CANVAS_WIDTH / ARR_LENGTH) - 1;
 for (let ind = 0; ind < ARR_LENGTH; ind++) {
-    ctx.fillRect((box_width+1)*ind, 0, box_width, randomArray[ind] / MAX_ARR_VAL);
+    ctx.fillRect((1+1)*ind, 0, 2, Math.floor(randomArray[ind] / MAX_ARR_VAL));
+    console.log((box_width+1)*ind)
 }
 
 // drawing the array on a canvas
