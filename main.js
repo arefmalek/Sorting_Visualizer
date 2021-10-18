28684ce262c9060b9225ce8614cc7bedfe81ff5d
arefmalek <arefmalek02@gmail.com>
2021-10-18 02:39:03 -0400

    both of them now work, kinda ugly but they work and I'll have to animate them soon

--- main.js
+++ main.js
@@ -6,7 +6,7 @@ myHeading.textContent = "hello yes!";
 let randomArray = [];
 let colors = [];
 var canvas = document.getElementById("myCanvas");
-const MAX_ARR_VAL = 1000;
+const MAX_ARR_VAL = 100;
 
 // COLORS
 const DEFAULT_COLOR = "black"
@@ -33,6 +33,10 @@ var speedOutput = document.getElementById("speedValue");
 speedOutput.innerHTML = speedSlider.value;
 var SPEED = speedSlider.value;
 
+// sorting
+var bubbleSort = document.getElementById("bubbleSort");
+var insertionSort = document.getElementById("insertionSort");
+
 speedSlider.oninput = function() {
     speedOutput.innerHTML = this.value;
     SPEED = this.value;
@@ -42,146 +46,87 @@ speedSlider.oninput = function() {
 // drawing the array on a canvas
 
 function resetArray() {
-    randomArray = []
+    randomArray = [];
+    colors = [];
     for (let i = 0; i < ARR_SIZE; i++) {
         randomArray.push(Math.floor(Math.random() * MAX_ARR_VAL));
         colors.push(DEFAULT_COLOR);
     }
+    console.log(randomArray);
     draw_array(canvas, randomArray, colors);
 }
 
-// this function isn't mine - Justin Johnson from Stanford wrote it
-// https://cs.stanford.edu/people/jcjohns/sorting.js/js/sorting.js
-function draw_array(canvas, ary, colors) {
-    /*
-     * Draw an array on a canvas.
-     *
-     * Inputs:
-     * - canvas: a DOM canvas object
-     * - ary: An array of numbers to draw
-     * - colors: An array of the same length as ary, whose ith element
-     *   is a string giving the color for the ith element of ary
+function draw_array(canvas, arr, colors) {
+    /**
+     * Draws the array sent on a canvas
+     * 
+     * canvas - DOM canvas element
+     * arr - array of elements
      */
-    var width_ratio = 2;
-    var ctx = canvas.getContext('2d');
 
-    // Clear the canvas
+    const width_ratio = 2;
+    let ctx = canvas.getContext("2d");
+
+    // clear the canvas
     ctx.fillStyle = '#fff';
-    ctx.fillRect(0, 0, canvas.width, canvas.height);
+    ctx.fillRect(0,0,canvas.width, canvas.height);
 
-    // Find min and max elements
-    var min = ary[0];
-    var max = ary[0];
-    for (var i = 1; i < ary.length; i++) {
-        min = ary[i] < min ? ary[i] : min;
-        max = ary[i] > max ? ary[i] : max;
-    }
+    const n = arr.length;
+    const spacing = canvas.width / (width_ratio * n + n + 1);
+    const bar_width = spacing * width_ratio;
 
-    // Figure out width of bars and spacing
-    var n = ary.length;
-    var spacing = canvas.width / (width_ratio * n + n + 1);
-    var bar_width = spacing * width_ratio;
+    // drawing rect around the outside of the canvas
+    ctx.strokeRect(0,0,canvas.width,canvas.height);
 
-    // Draw a box around the outside of the canvas
-    ctx.strokeRect(0, 0, canvas.width, canvas.height);
-
-    function convert_y(y) {
-        // Want convert_y(max) = 0, convert_y(min) = canvas.height`
-        var a = canvas.height / (min - max);
-        var b = max * canvas.height / (max - min);
-        return a * y + b;
-    }
-
-    // Draw a baseline for zero
-    var y_zero = convert_y(0);
-    ctx.beginPath();
-    ctx.moveTo(0, y_zero);
-    ctx.lineTo(canvas.width, y_zero);
-    ctx.stroke();
-
-    // Now draw boxes
-    var x = spacing;
-    for (var i = 0; i < ary.length; i++) {
+    // start drawing the boxes again
+    let x = spacing;
+    for (let i = 0; i < arr.length; i++) {
         ctx.fillStyle = colors[i];
-        var y = convert_y(ary[i]);
-        if (ary[i] >= 0) {
-            ctx.fillRect(x, y, bar_width, y_zero - y);
-        } else {
-            ctx.fillRect(x, y_zero, bar_width, y - y_zero);
-        }
+        const y = arr[i];
+        ctx.fillRect(x, 0, bar_width, y);
         x += spacing + bar_width;
     }
 }
 
-function compare_indexes(first, second) {
-    /**
-     * Compares two values 
-     * returns 1 if array value at first greater than array value at second
-     * returns -1 if array value at first less than array value at second
-     * 
-     * first - int index of the first element to compare
-     * second - int index of the second element to compare
-     */
-    colors[first] = COMPARE_COLOR;
-    colors[second] = COMPARE_COLOR;
-    draw_array(canvas, randomArray, colors);
-    sleep(SPEED);
-
-    var returnVal = 0
-    if (randomArray[first] >= randomArray[second]) {returnVal = 1;}
-    else {returnVal -1;}
-
-    colors[first] = DEFAULT_COLOR;
-    colors[second] = DEFAULT_COLOR;
- 
-}
-function sleep(ms) {
-    return new Promise(resolve => setTimeout(resolve, ms));
-}
- 
 function swap(index1, index2) {
-    /**
-     * Swaps the values between two arrays and updates the array
-     */
-    // update colors
-
-    // update values
-    temp = randomArray[index1];
+    const temp = randomArray[index1];
     randomArray[index1] = randomArray[index2];
     randomArray[index2] = temp;
 
-    // show new array
+    colors[index1] = SWAP_COLOR;
+    colors[index2] = SWAP_COLOR;
     draw_array(canvas, randomArray, colors);
-    sleep(SPEED);
-    return returnVal;
-}
+    setTimeout(SPEED);
 
-function insertion_sort() {
-    arr = randomArray
-    for (let i = 0; i < arr.length; i++) {
-        var minVal = i;
-        for (let j = i; j < arr.length; j++) {
-            if (arr[j] < arr[minVal]) { minval = j; }
-        }
-        if (minVal != i) {swap(i, minVal);}
-        draw_array(canvas, arr, colors);
-    }
-    console.log(arr === randomArray.sort())
-    return arr;
+    colors[index1] = DEFAULT_COLOR;
+    colors[index2] = DEFAULT_COLOR;
 }
 
 function bubble_sort() {
-    var compare = true;
+    let compare = true;
+    console.log(randomArray);
     while (compare === true) {
         compare = false;
-        for (let i = 1; i < randomArray.length; i++) {
-            console.log(compare_indexes(i-1, i));
-            draw_array(canvas, randomArray, colors)
-            if (compare_indexes(i-1, i) === 1) {
+        for (let i = 0; i < randomArray.length - 1; i++) {
+            if (randomArray[i] > randomArray[i+1]) {
                 compare = true;
-                swap(i, i-1);
-           }
+                swap(i, i+1);
+            }
         }
     }
-
+    draw_array(canvas, randomArray, colors);
 }
+
+function insertion_sort() {
+    for (let i = 0; i < randomArray.length; i++) {
+        let minDex = i;
+        for (let j = i + 1; j < randomArray.length; j++) {
+            if (randomArray[minDex] > randomArray[j]) {minDex = j;}
+        }
+        if (minDex != i) { swap(i, minDex);}
+    }
+    draw_array(canvas, randomArray, colors);
+}
+
+bubbleSort.addEventListener("click", bubble_sort);
+insertionSort.addEventListener("click", insertion_sort);
