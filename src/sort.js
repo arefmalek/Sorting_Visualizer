06e0b195f84863dfa4e5eaa8f2d6099dfed338b8
Aref Malek <malek.aref1@gmail.com>
2023-03-14 23:03:43 -0700

    mergesort doesn't work lmao

--- src/sort.js
+++ src/sort.js
@@ -1,192 +1,220 @@
-
 const isSorted = (array) => {
-    for (let i = 1; i < array.length; i++) {
-        if (array[i-1] > array[i]) {
-            return false;
-        }
+  for (let i = 1; i < array.length; i++) {
+    if (array[i - 1] > array[i]) {
+      return false;
     }
-    return true;
-}
+  }
+  return true;
+};
 
 const isMaxHeap = (array) => {
-    for (let i = 1; i < array.length; i++) {
-        if (array[Math.floor((i-1)/2)] < array[i]) {
-            return false;
-        }
+  for (let i = 1; i < array.length; i++) {
+    if (array[Math.floor((i - 1) / 2)] < array[i]) {
+      return false;
     }
-    return true;
-}
-
+  }
+  return true;
+};
 
 async function bubbleSort(arr) {
-    let compare = true;
-    while (compare === true) {
-        compare = false;
-        for (let i = 0; i < arr.length - 1; i++) {
-            // swap
-            if (arr[i] > arr[i+1]) {
-                compare = true;
-                // swap
-                swap(i, i+1);
-                drawBar(i, array[i], "red");
-                drawBar(i+1, array[i+1], "red");
-                await sleep();
-            }
-            drawArray();
-        }
+  let compare = true;
+  while (compare === true) {
+    compare = false;
+    for (let i = 0; i < arr.length - 1; i++) {
+      // swap
+      if (arr[i] > arr[i + 1]) {
+        compare = true;
+        // swap
+        swap(i, i + 1);
+        drawBar(i, array[i], "red");
+        drawBar(i + 1, array[i + 1], "red");
+        await sleep();
+      }
+      drawArray();
     }
+  }
 
-    return arr;
+  return arr;
 }
 
 async function selectionSort(arr) {
-    // algorithm works by selecting the minimum element in i...n and swapping
-    for (let i = 0; i < arr.length; i++) {
-        let minDex = i;
-        for (let j = i+1; j < arr.length; j++) {
+  // algorithm works by selecting the minimum element in i...n and swapping
+  for (let i = 0; i < arr.length; i++) {
+    let minDex = i;
+    for (let j = i + 1; j < arr.length; j++) {
+      drawArray();
+      drawBar(minDex, array[minDex], "red");
+      if (arr[minDex] > arr[j]) {
+        // update smallest element
+        minDex = j;
 
-            drawArray();
-            drawBar(minDex, array[minDex], "red");
-            if (arr[minDex] > arr[j]) {
-                // update smallest element
-                minDex = j;
-
-                drawBar(minDex, array[minDex], "red");
-                // await sleep();
-            }
-            await sleep();
-        }
-
-        // swap i with minDex
-        drawArray();
-        drawBar(i, array[i], "red");
         drawBar(minDex, array[minDex], "red");
-        swap(i, minDex);
-        await sleep();
-        drawArray();
+        // await sleep();
+      }
+      await sleep();
     }
 
-    return arr;
+    // swap i with minDex
+    drawArray();
+    drawBar(i, array[i], "red");
+    drawBar(minDex, array[minDex], "red");
+    swap(i, minDex);
+    await sleep();
+    drawArray();
+  }
+
+  return arr;
 }
 
 async function insertionSort(arr) {
-    for (let i = 1; i < arr.length; i++) {
-        // start at first index
-        let j = i;
-        while (j > 0 && arr[j-1] > arr[j]) { 
+  for (let i = 1; i < arr.length; i++) {
+    // start at first index
+    let j = i;
+    while (j > 0 && arr[j - 1] > arr[j]) {
+      drawArray();
+      drawBar(j - 1, arr[j - 1], "red");
+      drawBar(j, arr[j], "red");
+      swap(j - 1, j);
+      await sleep();
 
-            drawArray();
-            drawBar(j-1, arr[j-1], "red");
-            drawBar(j, arr[j], "red");
-            swap(j-1, j);
-            await sleep();
-
-            j--;
-        }
+      j--;
     }
+  }
 
-    return arr;
+  return arr;
 }
 
 /**
- * 
- * @param {Array to construct heap from, then sort} arr 
+ *
+ * @param {Array to construct heap from, then sort} arr
  */
 async function heapSort(arr) {
-    const getLeftIndex = (index) => {
-        return (index * 2 + 1 < arr.length) ? index * 2 + 1: -1;
+  const getLeftIndex = (index) => {
+    return index * 2 + 1 < arr.length ? index * 2 + 1 : -1;
+  };
+
+  const getRightIndex = (index) => {
+    return index * 2 + 2 < arr.length ? index * 2 + 2 : -1;
+  };
+
+  const getParentIndex = (index) => {
+    return index != 0 ? Math.floor((index - 1) / 2) : -1;
+  };
+
+  // construct maxheap: swim up for each index
+  for (let index = 1; index < arr.length; index++) {
+    let parentIndex = getParentIndex(index);
+    // swim up until we find place in Max-Heap
+    while (parentIndex != -1 && arr[parentIndex] < arr[index]) {
+      // do the swap and update the value
+      // console.log(parentIndex, index);
+
+      drawArray();
+      drawBar(index, arr[index], "red");
+      drawBar(parentIndex, arr[parentIndex], "red");
+
+      swap(parentIndex, index);
+      await sleep();
+      drawArray();
+
+      // info for next iteration
+      index = parentIndex;
+      parentIndex = getParentIndex(index);
     }
+  }
 
-    const getRightIndex = (index) => {
-        return (index * 2 + 2 < arr.length) ? index * 2 + 2: -1;
+  // removeMax and re-establish heap
+  for (let i = arr.length - 1; i > 0; i--) {
+    // swap maxNode with end of the heap
+    drawArray();
+    drawBar(0, arr[0], "red");
+    drawBar(i, arr[i], "red");
+
+    swap(0, i);
+    await sleep();
+    drawArray();
+
+    // utility functions to get left and right children
+    const getLeftChild = (index) => {
+      return index * 2 + 1 < i ? arr[index * 2 + 1] : -1;
+    };
+    const getRightChild = (index) => {
+      return index * 2 + 2 < i ? arr[index * 2 + 2] : -1;
+    };
+
+    // sink down the node until in the correct position
+    let index = 0;
+    while (
+      getLeftIndex(index) < i && // heap has at least 1 child node (left firs always)
+      (arr[index] < getLeftChild(index, i) ||
+        arr[index] < getRightChild(index, i))
+    ) {
+      // swap index with larger of two children
+      let newIndex =
+        getLeftChild(index, i) > getRightChild(index, i)
+          ? getLeftIndex(index)
+          : getRightIndex(index);
+
+      drawArray();
+      drawBar(index, arr[index], "red");
+      drawBar(newIndex, arr[newIndex], "red");
+
+      swap(index, newIndex);
+      await sleep();
+      drawArray();
+
+      index = newIndex;
     }
+  }
 
-    const getParentIndex = (index) => {
-        return (index != 0) ? Math.floor((index-1)/2): -1
-    }
-
-    // construct maxheap: swim up for each index
-    for (let index = 1; index < arr.length; index++) {
-
-        let parentIndex = getParentIndex(index);
-        // swim up until we find place in Max-Heap
-        while (parentIndex != -1 && arr[parentIndex] < arr[index]) {
-            // do the swap and update the value
-            // console.log(parentIndex, index);
-
-            drawArray();
-            drawBar(index, arr[index], "red");
-            drawBar(parentIndex, arr[parentIndex], "red");
-
-            swap(parentIndex, index);
-            await sleep();
-            drawArray();
-
-            // info for next iteration
-            index = parentIndex;
-            parentIndex = getParentIndex(index);
-        }
-    }
-
-    // removeMax and re-establish heap
-    for (let i = arr.length-1; i > 0; i--) {
-        // swap maxNode with end of the heap
-        drawArray();
-        drawBar(0, arr[0], "red");
-        drawBar(i, arr[i], "red");
-
-        swap(0, i);
-        await sleep();
-        drawArray();
-
-        // utility functions to get left and right children 
-        const getLeftChild = (index) => {
-            return (index * 2 + 1 < i) ? arr[index * 2 + 1]: -1;
-        }
-        const getRightChild = (index) => {
-            return (index * 2 + 2 < i) ? arr[index * 2 + 2]: -1;
-        }
-
-        // sink down the node until in the correct position
-        let index = 0;
-        while ((getLeftIndex(index) < i) && // heap has at least 1 child node (left firs always)
-                (arr[index] < getLeftChild(index, i) || arr[index] < getRightChild(index, i))
-        ) {
-            // swap index with larger of two children
-            let newIndex = (getLeftChild(index, i) > getRightChild(index, i)) ? getLeftIndex(index) : getRightIndex(index);
-
-            drawArray();
-            drawBar(index, arr[index], "red");
-            drawBar(newIndex, arr[newIndex], "red");
-
-            swap(index, newIndex);
-            await sleep();
-            drawArray();
-
-            index = newIndex;
-        }
-    }
-
-    return arr;
+  return arr;
 }
 
+// async function mergeSort(arr) {}
+async function recursiveMergeSort(arr) {
+  if (arr.length == 1) {
+    console.log("base case", arr);
+    return arr;
+  }
+  const half = Math.ceil(arr.length / 2);
+  let left = recursiveMergeSort(arr.slice(0, half));
+  let right = recursiveMergeSort(arr.slice(half));
 
+  let [leftIndex, rightIndex, i] = [0, 0, 0];
 
-async function mergeSort(leftIndex, rightIndex) {
+  while (i < arr.length) {
+    if (leftIndex == left.length || left[leftIndex] > right[rightIndex]) {
+      arr.append(right[rightIndex]);
+      rightIndex++;
+    } else if (
+      rightIndex == right.length ||
+      right[rightIndex] >= left[leftIndex]
+    ) {
+      arr.append(left[leftIndex]);
+      leftIndex++;
+    } else {
+      //   console.log("whoopsies");
+      leftIndex++;
+      rightIndex++;
+    }
+    i++;
+  }
+
+  return arr;
 }
 
 function test() {
-    rand_arr = [];
-    let s = 20                                      
-    let maxVal = 100
-    // build random array
-    for(let i = 0; i < s; i++) {
-        rand_arr.push(Math.floor(Math.random()*maxVal));
-    }
+  rand_arr = [];
+  let s = 20;
+  let maxVal = 100;
+  // build random array
+  for (let i = 0; i < s; i++) {
+    rand_arr.push(Math.floor(Math.random() * maxVal));
+  }
 
-    // test sorting algo
-    bubble_sort(rand_arr).then(result => {
-        console.log(result);
-        console.log(isSorted(result));
-    });
+  // test sorting algo
+  bubble_sort(rand_arr).then((result) => {
+    console.log(result);
+    console.log(isSorted(result));
+  });
 }
